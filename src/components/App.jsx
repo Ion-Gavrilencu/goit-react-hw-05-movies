92e51f779fd6ce937294fb65e8fb23ee0c3ed63f
Ion-Gavrilencu <157795149+Ion-Gavrilencu@users.noreply.github.com>
2024-09-18 16:19:16 +0300

    update 01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom'; 
+import { Routes, Route, } from 'react-router-dom'; 
 import { lazy } from 'react';
 import { Toaster } from 'react-hot-toast'; 
 
